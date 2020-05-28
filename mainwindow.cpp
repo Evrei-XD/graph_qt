c389f2e328e1bf575698fbad641750df236e8d65
Evrei-XD <rom_ser2011@mail.ru>
2020-05-28 18:05:49 +0300

    18:05 Графики с добавлением точек

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    MainWindow::makePlot();
 }
 
 MainWindow::~MainWindow()
@@ -15,9 +16,8 @@ MainWindow::~MainWindow()
 
 void MainWindow::makePlot()
 {
-    // generate some data:
     QVector<double> x(101), y(101); // initialize with entries 0..100
-    for (int i=0; i<101; ++i)
+    for (int i=0; i<100; ++i)
     {
       x[i] = i/50.0 - 1; // x goes from -1 to 1
       y[i] = x[i]*x[i]; // let's plot a quadratic function
