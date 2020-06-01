39c10fc07304fb8e8ba5d837e150de4bd5e0a832
Evrei-XD <rom_ser2011@mail.ru>
2020-06-01 17:20:15 +0300

    17:18 Коммит в новую ветку. Рабочий график с генерацией случайных чисел. Но подлагивающий

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,12 +1,93 @@
+/*
+* mainwindow.cpp
+*
+*  Created on  : Dec 10, 2017
+*  Author      : Vinay Divakar
+*  website     : www.deeplyemebedded.org
+*/
+
+#include<QDebug>
+#include<QThread>
+#include<QFont>
+#include <random>
+#include <QRandomGenerator>
+#include <QDebug>
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "ad7190.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+/* Global objects */
+//SPI spi_device1;
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    MainWindow::makePlot();
+    setWindowFlags(Qt::FramelessWindowHint);
+    setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
+//    QWSServer::setCursorVisible(false);
+    this->setStyleSheet("background-color: white;");
+
+    /* Initialize SPI */
+//    if (spi_device1.SPI_DEV1_init(SPIDEV_BYTES_NUM, SPIDEV1_BUS_SPEED_HZ, SPI_SS_LOW,
+//                                  SPIDEV_DELAY_US, SPIDEV_DATA_BITS_NUM,
+//                                  SPI_MODE3) == -1)
+//        qDebug()<<"(Main)spidev1.0 initialization failed\r\n";
+
+//    else
+//        qDebug()<<"(Main)spidev1.0 initialized - READY\r\n";
+
+    /* Configure font settings for Label */
+    QFont font;
+    font.setPointSize(21);
+    font.setItalic(true);
+    font.setBold(true);
+    ui->label_adc_data->setFont(font);
+    ui->label_adc_data->setAlignment(Qt::AlignCenter);
+    ui->label_adc_data->setStyleSheet("background-color : white; color : green;");
+
+    /* Configure setup for Thank You Label */
+    ui->label_ty->setAlignment(Qt::AlignCenter);
+    ui->label_ty->setStyleSheet("background-color : white; color : black;");
+
+    /* Thread to continously sample and read the data */
+//    QThread *thread = new QThread;
+//    AD7190 *ad7190_dev = new AD7190();
+//    ad7190_dev->moveToThread(thread);
+//    connect(thread,SIGNAL(started()),ad7190_dev,SLOT(AD7190_init_thread()));
+//    connect(ad7190_dev, SIGNAL(new_adc_data(ulong)),this,SLOT(display_data(ulong)));
+//    thread->start();
+
+    /* Add graph and set the curve line color to green */
+    ui->CustomPlot->addGraph();
+    ui->CustomPlot->graph(0)->setPen(QPen(Qt::red));
+    ui->CustomPlot->graph(0)->setAntialiasedFill(false);
+
+    /* Configure x-Axis as time in secs */
+    QSharedPointer<QCPAxisTickerTime> timeTicker(new QCPAxisTickerTime);
+    timeTicker->setTimeFormat("%s");
+    ui->CustomPlot->xAxis->setTicker(timeTicker);
+    ui->CustomPlot->axisRect()->setupFullAxesBox();
+
+
+    /* Configure x and y-Axis to display Labels */
+    ui->CustomPlot->xAxis->setTickLabelFont(QFont(QFont().family(),8));
+    ui->CustomPlot->yAxis->setTickLabelFont(QFont(QFont().family(),8));
+    ui->CustomPlot->xAxis->setLabel("Time(s)");
+    ui->CustomPlot->yAxis->setLabel("ADC Raw Counts");
+
+    /* Make top and right axis visible, but without ticks and label */
+    ui->CustomPlot->xAxis2->setVisible(true);
+    ui->CustomPlot->yAxis->setVisible(true);
+    ui->CustomPlot->xAxis2->setTicks(false);
+    ui->CustomPlot->yAxis2->setTicks(false);
+    ui->CustomPlot->xAxis2->setTickLabels(false);
+    ui->CustomPlot->yAxis2->setTickLabels(false);
+
+    /* Set up and initialize the graph plotting timer */
+    connect(&timer_plot, SIGNAL(timeout()),this,SLOT(realtimePlot()));
+    timer_plot.start(5);
 }
 
 MainWindow::~MainWindow()
@@ -14,65 +95,62 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::setupePlot()
+/****************************************************************
+ * Function Name : display_data
+ * Description   : Displays ADC data on the GUI
+ * Returns       : None
+ * Params        @adc_data: Data to be displayed
+ ****************************************************************/
+void MainWindow::display_data()
 {
-    ui->customPlot->addGraph(); // blue line
-    ui->customPlot->graph(0)->setPen(QPen(QColor(40, 110, 255)));
-    ui->customPlot->addGraph(); // red line
-    ui->customPlot->graph(1)->setPen(QPen(QColor(255, 110, 40)));
+//    генерируем рандомный лонг для отображения
+    unsigned long data = 1353;
+    /* Discard the noisy bits */
+//    adc_data = adc_data/100;
 
-//    QSharedPointer<QCPAxisTickerTime> timeTicker(new QCPAxisTickerTime);
-//    timeTicker->setTimeFormat("%m:%s");
-////    QDateTime::currentDateTime().toString("hh-mm-ss");
-//    ui->customPlot->xAxis->setTicker(timeTicker);
-    ui->customPlot->axisRect()->setupFullAxesBox();
-    ui->customPlot->yAxis->setRange(-1.2, 1.2);
+    /* Support the plotter */
+    adc_data_g = data;//adc_data;
 
-    // make left and bottom axes transfer their ranges to right and top axes:
-    connect(ui->customPlot->xAxis, SIGNAL(rangeChanged(QCPRange)), ui->customPlot->xAxis2, SLOT(setRange(QCPRange)));
-    connect(ui->customPlot->yAxis, SIGNAL(rangeChanged(QCPRange)), ui->customPlot->yAxis2, SLOT(setRange(QCPRange)));
-
-    // setup a timer that repeatedly calls MainWindow::realtimeDataSlot:
-    dataTimer = new QTimer();
-//    connect(dataTimer, SIGNAL(timeout()), this, SLOT(slotTimerAlarm()));
-//    dataTimer->start(1000); // И запустим таймер
-    connect(dataTimer, SIGNAL(timeout()), this, SLOT(realtimeDataSlot()));
-    dataTimer->start(10); // Interval 0 means to refresh as fast as possible
+    /* Prepare to display the data */
+    QString adc_data_str = " ";
+    adc_data_str = QString::number(data);
+    ui->label_adc_data->setText(adc_data_str);
 }
 
-
-void MainWindow::makePlot()
+/****************************************************************
+ * Function Name : realtimePlot
+ * Description   : Displays the real time graph on the GUI
+ * Returns       : None
+ * Params        : None
+ ****************************************************************/
+void MainWindow::realtimePlot()
 {
     static QTime time(QTime::currentTime());
-    // calculate two new data points:
-    double key = time.elapsed()/1000.0; // time elapsed since start of demo, in seconds
+    double key = time.elapsed()/1000.0;//100
     static double lastPointKey = 0;
-    if (key-lastPointKey > 0.002) // at most add point every 2 ms
+    double data = 0;
+    if(key - lastPointKey > 0.033)//0.002
     {
-      // add data to lines:
-      ui->customPlot->graph(0)->addData(key, qSin(key)+qrand()/(double)RAND_MAX*1*qSin(key/0.3843));
-      ui->customPlot->graph(1)->addData(key, qCos(key)+qrand()/(double)RAND_MAX*0.5*qSin(key/0.4364));
-      // rescale value (vertical) axis to fit the current data:
-//      ui->customPlot->graph(0)->rescaleValueAxis();
-//      ui->customPlot->graph(1)->rescaleValueAxis(true);
-      lastPointKey = key;
+        data = QRandomGenerator::global()->generateDouble();
+//        qDebug() << QString::number(data);
+//        adc_data_g = data;
+        ui->CustomPlot->graph(0)->addData(key, data);
+        lastPointKey = key;
     }
-    // make key axis range scroll with the data (at a constant range size of 8):
-    ui->customPlot->xAxis->setRange(key, 8, Qt::AlignRight);
-    ui->customPlot->replot();
 
-    // calculate frames per second:
-    static double lastFpsKey;
-    static int frameCount;
-    ++frameCount;
-    if (key-lastFpsKey > 2) // average fps over 2 seconds
-    {
-      ui->statusBar->showMessage(
-            QString("%1 FPS, Total Data points: %2")
-            .arg(frameCount/(key-lastFpsKey), 0, 'f', 0)
-            .arg(ui->customPlot->graph(0)->data()->size()+ui->customPlot->graph(1)->data()->size())
-            , 0);
-      lastFpsKey = key;
-      frameCount = 0;
-    }
+    /* make key axis range scroll right with the data at a constant range of 8. */
+    ui->CustomPlot->graph(0)->rescaleValueAxis();
+    ui->CustomPlot->xAxis->setRange(key, 5, Qt::AlignRight);
+    ui->CustomPlot->replot();
+}
+
+/****************************************************************
+ * Function Name : on_pushButton_released
+ * Description   : Displays THANK YOU! when pressed and released
+ * Returns       : None
+ * Params        : None
+ ****************************************************************/
+void MainWindow::on_pushButton_released()
+{
+    ui->label_ty->setText("THANK YOU!");
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,32 +1,44 @@
+/*
+* mainwindow.h
+*
+*  Created on  : Dec 10, 2017
+*  Author      : Vinay Divakar
+*  website     : www.deeplyemebedded.org
+*/
+
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+/* Includes */
 #include <QMainWindow>
-#include <QTimer>
-#include <QTime>
-#include <QDateTime>
-#include <QElapsedTimer>
+#include<QTimer>
 
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
 private slots:
-    void makePlot();
-    void setupePlot();
+
+    void on_pushButton_released();
+
+public slots:
+//    void display_data(unsigned long adc_data);
+    void display_data();
+    void realtimePlot();
+
 
 private:
     Ui::MainWindow *ui;
-    QTimer *dataTimer;
-//    QSharedPointer *timeTicker;
-//    QCPAxisTickerTime *timeTicker;
+    unsigned long adc_data_g;
+    QTimer timer_plot;
 };
+
 #endif // MAINWINDOW_H
